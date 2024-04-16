b9b90a98c9365e1a2fe9560af7c3e0dcd9f09592
ParthSareen <psareen@uwaterloo.ca>
2024-04-16 00:18:42 -0400

    Test fix

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,6 +1,6 @@
 import json
-# from extensilogger import ExtensiLogger
-from extensilogger.extensilogger import ExtensiLogger
+from extensilogger import ExtensiLogger
+# from extensilogger.extensilogger import ExtensiLogger
 
 # Clear the event_log.json file
 with open('event_log.json', 'w') as file:
