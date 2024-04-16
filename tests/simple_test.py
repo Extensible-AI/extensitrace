20718d05c36357f0fd8cee88a812aa03e0c60ddd
ParthSareen <psareen@uwaterloo.ca>
2024-04-16 10:44:20 -0400

    Improving tests

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -8,15 +8,29 @@ with open('event_log.json', 'w') as file:
 
 logger = ExtensiLogger()
 
-@logger.log()
+@logger.log(track=True)
 def test():
+    test2()
+    pass
+
+@logger.log()
+def test2():
+    pass
+
+@logger.log(track=True)
+def test3():
     pass
 
 if __name__ == '__main__':
     test()
+    test3()
     
     # Load event_log.json and make sure the length is one
     with open('event_log.json', 'r') as file:
         log_data = json.load(file)
-        assert len(log_data) == 1, "The length of event_log.json should be 1"
+        task_ids = [entry['task_id'] for entry in log_data if 'task_id' in entry]
+        print(f"Total log entries: {len(log_data)}")
+        print(f"Total unique task_ids: {len(set(task_ids))}")
+        assert len(log_data) == 3, "The length of event_log.json should be 3"
+        assert len(set(task_ids)) == 2, "There should be 3 unique task_ids"
         print('Simple test passed!')
