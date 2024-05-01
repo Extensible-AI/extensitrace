7025a724ea07a4f8f3131186329c8036c1990cc1
ParthSareen <psareen@uwaterloo.ca>
2024-05-01 10:56:21 -0400

    adding setup changes

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='extensilog',
     version='0.0.1',
     packages=find_packages(include=['extensilog', 'extensilog.*']),
     install_requires=required,
+    description='A logger for tracking your agent workflow',
+    long_description=long_description,
+    license='Apache License 2.0',
     author='Parth Sareen, Omkaar Kamath',
     author_email='parth@extensible.dev, omkaar@extensible.dev',
     description='A logger for tracking your agent workflow',
