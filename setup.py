c9151f296fdf8d31210ac053d9c855ad57c936a5
ParthSareen <psareen@uwaterloo.ca>
2024-06-09 18:28:59 -0400

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='extensitrace',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(include=['extensitrace', 'extensitrace.*']),
     install_requires=required,
     long_description=long_description,
@@ -23,5 +23,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.10',
 )
